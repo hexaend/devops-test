d174045e93f4c9b71b0d4c25ff0b7796dbe29b8d
hexaend <hexaend@mail.ru>
2025-12-02 23:49:33 +0300

    change workflow.yml

--- demo/build.gradle.kts
+++ demo/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     java
     id("org.springframework.boot") version "4.0.0"
     id("io.spring.dependency-management") version "1.1.7"
+    jacoco
 }
 
 group = "ru.hexaend"
@@ -37,4 +38,57 @@ dependencies {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+
+    classDirectories.setFrom(files(classDirectories.files.map {
+        fileTree(it) {
+            exclude(
+                "**/config/**",
+                "**/entity/**",
+                "**/dto/**",
+                "**/DemoApplication.class"
+            )
+        }
+    }))
+
+    reports {
+        xml.required.set(true)
+        html.required.set(true)
+        csv.required.set(false)
+    }
+}
+
+tasks.jacocoTestCoverageVerification {
+    violationRules {
+        rule {
+            element = "PACKAGE"
+            excludes = listOf("*. config", "*.entity", "*.dto")
+
+            limit {
+                counter = "LINE"
+                value = "COVEREDRATIO"
+                minimum = "0.80".toBigDecimal()
+            }
+        }
+
+        rule {
+            element = "CLASS"
+            excludes = listOf("*Test")
+
+            limit {
+                counter = "LINE"
+                value = "COVEREDRATIO"
+                minimum = "0.75".toBigDecimal()
+            }
+        }
+    }
+}
+
+tasks.test {
+    finalizedBy(tasks.jacocoTestReport)
 }
